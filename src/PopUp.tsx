9400bcd4af2c92f10684881924d660054cb78af4
Yichuan Shen <shen.yichuan@gmail.com>
2020-04-18 17:13:06 +0200

    Add PopUp component

---
+++ src/PopUp.tsx
@@ -0,0 +1,47 @@
+import {createElement, createRef, Component, MouseEvent} from 'react'
+import classnames from 'classnames'
+import {PortuiComponentProps} from './main'
+
+export interface PopUpProps extends PortuiComponentProps {
+  show?: boolean
+
+  onBackdropClick?: (evt: MouseEvent) => any
+}
+
+export default class PopUp extends Component<PopUpProps> {
+  elementRef = createRef<HTMLDivElement>()
+
+  handleClick = (evt: MouseEvent) => {
+    if (evt.target !== this.elementRef.current) return
+
+    this.props.onBackdropClick?.(evt)
+  }
+
+  render() {
+    let {props} = this
+
+    return (
+      <div
+        ref={this.elementRef}
+        id={props.id}
+        className={classnames('portui-pop-up', props.className, {
+          'portui-show': props.show,
+        })}
+        style={{
+          position: 'fixed',
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          display: props.show ? 'flex' : 'none',
+          justifyContent: 'center',
+          alignItems: 'center',
+          ...props.style,
+        }}
+        onClick={this.handleClick}
+      >
+        {props.children}
+      </div>
+    )
+  }
+}
---
+++ stories/PopUp.stories.tsx
@@ -0,0 +1,30 @@
+import {createElement, Fragment, useState} from 'react'
+import {action} from '@storybook/addon-actions'
+import PopUp from '../src/PopUp'
+
+export default {
+  title: 'PopUp',
+  component: PopUp,
+}
+
+export const Default = () => {
+  let [showPopUp, setShowPopUp] = useState(false)
+
+  return (
+    <Fragment>
+      <button onClick={evt => setShowPopUp(true)}>Open PopUp</button>
+
+      <PopUp
+        style={{background: 'rgba(0, 0, 0, .1)'}}
+        show={showPopUp}
+        onBackdropClick={action('onBackdropClick')}
+      >
+        <div style={{background: 'rgba(0, 0, 0, .1)'}}>
+          Hello World!
+          <br />
+          <button onClick={evt => setShowPopUp(false)}>Close</button>
+        </div>
+      </PopUp>
+    </Fragment>
+  )
+}
